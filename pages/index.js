e82da91369f68558c0995606a0672d3a2f812264
LAJaldon <lajaldon@gmail.com>
2023-08-07 20:54:37 +1000

    added production text

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -13,6 +13,8 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <h2>This is the production site!</h2>
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
@@ -55,7 +57,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
@@ -111,5 +113,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
